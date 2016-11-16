3010718b8e3a6a3db634ab79f16e2a7ea71353ce
LarsZondag <larszondag@gmail.com>
2016-11-16 09:42:03 +0100

    Some translations and viewport adjustment

--- src/components/SurvivalStrijd.vue
+++ src/components/SurvivalStrijd.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-    <p>Zaterdag 21 mei organiseert SBV Slopend de Survivalstrijd, de jaarlijkse run over de campus van de TU! Heb jij het in je om alle hindernissen te overwinnen?</p>
+    <p>{{ $t('survivalstrijd.beforeVideo') }}</p>
     <div class="videoWrapper">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/74n7mKQ4rt8" frameborder="0" allowfullscreen></iframe>
     </div>
-    <p>De inschrijvingen zijn gesloten. Wil je toch mee doen? Schrijf je dan op 21 mei vòòr 14:00 in op het sportcentrum. De plaatsen zijn beperkt, wees er snel bij! / Subscriptions for the Survival Strijd are closed. If you still want to participate, subscribe on the 21 of may before 14:00 at the sports centre. Extra places are limited, so be on time!</p> 
+    <p>{{ $t('survivalstrijd.afterVideo') }}</p> 
     </div>
 </template>
 
--- src/components/WatIsEenSurvivalrun.vue
+++ src/components/WatIsEenSurvivalrun.vue
@@ -1,12 +1,18 @@
 <template>
 <div>
-<p>Wat is een survivalrun? Een survivalrun is een wedstrijd waarbij je zo snel mogelijk een parcours met hindernissen af moet leggen. Deze hindernissen variëren van touwklimmen tot boogschieten en alles wat daar tussen in zit.
-FAQ</p>
+<p></p>
 
-<p>Wat voor kleding moet ik aan? Gewone sportkleding en (hardloop)schoenen zijn goed genoeg. Een lange broek is handig vanwege de touwen bij de hindernissen, een (hardloop)tight is helemaal goed. Wij zorgen voor een mooi T-shirt met je startnummer.
-Wat kan ik winnen? Voor de snelste heer en de snelste dame individueel hebben we een 10-rittenkaart om te gaan boulderen bij Delfts Bleau. Het snelste team en het team met de leukste outfit wint een bierproeverij bij de Koperen Kat!
-Is er nog iets anders te doen als ik gefinisht ben? Bij de finish staat er eerst een warme hottub op je te wachten. Dan kun je nog even nagenieten bij de barbecue!
-Ik ben overtuigd, hoe kan ik me inschrijven? Inschrijven voor de korte run kost €9,00 en kan via de knop: deelnemersinformatie en inschrijven</p>
+<p>{{ $t('watIsEenSurvivalrun.watIsHet') }}</p>
+
+<p>FAQ</p>
+
+<p>{{ $t('watIsEenSurvivalrun.watVoorKleding') }}</p>
+
+<p>{{ $t('watIsEenSurvivalrun.watKanIkWinnen') }}</p>
+
+<p>{{ $t('watIsEenSurvivalrun.isErNogIets') }}</p>
+
+<p>{{ $t('watIsEenSurvivalrun.ikBenOvertuigd') }}</p>
 
 </div>
 </template>
